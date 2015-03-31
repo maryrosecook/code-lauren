3fcc9de95bed94a3272ed0f335599e6ca732a50e
Mary Rose Cook <mary@maryrosecook.com>
2015-03-31 14:27:37 -0400

    Set editor font to courier 11px

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ function setupEditor() {
   editor.clearSelection(); // for some reason, setting the initial text selects everything
   editor.focus();
   editor.setTheme('ace/theme/monokai');
+
+  editor.setOptions({
+    fontFamily: "courier",
+    fontSize: "11pt"
+  });
+
   editor.renderer.setShowGutter(false);
   editor.getSession().setTabSize(2);
   editor.getSession().setUseSoftTabs(true);
