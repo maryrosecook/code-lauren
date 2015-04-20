bf42293bae819e6e341c34cd58eef025bfa116e5
Mary Rose Cook <mary@maryrosecook.com>
2015-04-20 19:19:20 -0400

    Indent test program nicely

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var createEditor = require("./editor");
 var createEnv = require("./env");
 
 window.addEventListener("load", function() {
-  var editor = createEditor('(let [draw { ?r (draw-filled-circle 250 250 r "red") }\n      update { ?r \n        (draw r) \n        (update (add r 1))\n      }]       \n\n (update 0))');
+  var editor = createEditor('(let [draw { ?r\n        (clear-screen)\n        (draw-filled-circle 250 250 r "red")\n      }\n\n      update { ?r \n        (draw r) \n        (update (add r 2))\n      }]       \n\n (update 0))');
   var screen = document.getElementById("screen").getContext("2d");
   var env = lang.createScope(createEnv(screen)); // will get mutated for now
 
