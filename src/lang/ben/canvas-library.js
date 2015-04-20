dd8da9adee32a720f01eb722c20238cbd10200fd
Mary Rose Cook <mary@maryrosecook.com>
2015-04-20 18:53:23 -0400

    Make canvas fns generators

--- src/lang/ben/canvas-library.js
+++ src/lang/ben/canvas-library.js
@@ -2,17 +2,17 @@ var _ = require("underscore");
 
 var canvasLibrary = module.exports = function(screen) {
   return {
-    "write-text": function(str, x, y, color) {
+    "write-text": function*(str, x, y, color) {
       screen.font = "20px Georgia";
       screen.fillStyle = color;
       screen.fillText(str, x, y);
     },
 
-    "clear-screen": function() {
+    "clear-screen": function*() {
       screen.clearRect(0, 0, screen.canvas.width, screen.canvas.height);
     },
 
-    "draw-unfilled-circle": function(x, y, radius, color) {
+    "draw-unfilled-circle": function*(x, y, radius, color) {
       screen.beginPath();
       screen.arc(x, y, radius, 0, Math.PI * 2, true);
       screen.closePath();
@@ -20,7 +20,7 @@ var canvasLibrary = module.exports = function(screen) {
       screen.stroke();
     },
 
-    "draw-filled-circle": function(x, y, radius, color) {
+    "draw-filled-circle": function*(x, y, radius, color) {
       screen.beginPath();
       screen.arc(x, y, radius, 0, Math.PI * 2, true);
       screen.closePath();
@@ -28,12 +28,12 @@ var canvasLibrary = module.exports = function(screen) {
       screen.fill();
     },
 
-    "draw-unfilled-rectangle": function(x, y, width, height, color) {
+    "draw-unfilled-rectangle": function*(x, y, width, height, color) {
       screen.strokeStyle = color;
       screen.strokeRect(x, y, width, height);
     },
 
-    "draw-filled-rectangle": function(x, y, width, height, color) {
+    "draw-filled-rectangle": function*(x, y, width, height, color) {
       screen.fillStyle = color;
       screen.fillRect(x, y, width, height);
     }
