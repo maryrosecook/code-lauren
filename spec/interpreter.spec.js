aa7b5c05d4ebdb42bb2a4e0e0dcfdda702002c29
Mary Rose Cook <mary@maryrosecook.com>
2015-05-17 23:09:24 -0400

    Update all interpreter tests to call parse on input
    * Runner now expects an AST

--- spec/interpreter.spec.js
+++ spec/interpreter.spec.js
@@ -1,6 +1,7 @@
 var i = require("../src/lang/interpreter");
 var standardLibrary = require("../src/lang/standard-library");
 
+var p = require("../src/lang/parser");
 var r = require("../src/runner");
 var c = r.complete;
 var step = r.step;
@@ -8,27 +9,27 @@ var step = r.step;
 describe("interpreter", function() {
   describe("env", function() {
     it("should be able to interpret if no env passed in", function() {
-      expect(c(r("1"))).toEqual(1);
+      expect(c(r(p("1")))).toEqual(1);
     });
   });
 
   describe("literals", function() {
     it("should be able to produce a number", function() {
-      expect(c(r("1"))).toEqual(1);
+      expect(c(r(p("1")))).toEqual(1);
     });
 
     it("should be able to produce a string", function() {
-      expect(c(r('"hi my name is lauren"'))).toEqual("hi my name is lauren");
+      expect(c(r(p('"hi my name is lauren"')))).toEqual("hi my name is lauren");
     });
 
     it("should be able to produce a string", function() {
-      expect(c(r("true"))).toEqual(true);
+      expect(c(r(p("true")))).toEqual(true);
     });
   });
 
   describe("pausing execution", function() {
     it("should be able to pause before execution of a lambda", function() {
-      var g = r("{1}({2}())");
+      var g = r(p("{1}({2}())"));
       expect(step(g)).toEqual(null);
       expect(step(g)).toEqual(null);
       expect(step(g)).toEqual(1);
@@ -38,60 +39,60 @@ describe("interpreter", function() {
   describe("fn invocation", function() {
     describe("library fns", function() {
       it("should be able to call a library fn with no args", function() {
-        expect(c(r(('print()')))).toEqual("\n");
+        expect(c(r(p(('print()'))))).toEqual("\n");
       });
 
       it("should be able to call a library fn with some args", function() {
-        expect(c(r(('add(1 2 3)')))).toEqual(6);
+        expect(c(r(p('add(1 2 3)')))).toEqual(6);
       });
 
       it("should be able to invoke all invocations", function() {
-        expect(c(r('print("a")\nprint("b")'))).toEqual("b\n");
+        expect(c(r(p('print("a")\nprint("b")')))).toEqual("b\n");
       });
 
       it("should be able to call fn that uses thet result of invocation as arg", function() {
-        expect(c(r(('add(1 add(2 3) 4)')))).toEqual(10);
+        expect(c(r(p('add(1 add(2 3) 4)')))).toEqual(10);
       });
     });
 
     describe("lambdas", function() {
       it("should be able to invoke a lambda on no args", function() {
-        expect(c(r('{ 1 }()'))).toEqual(1);
+        expect(c(r(p('{ 1 }()')))).toEqual(1);
       });
 
       it("should be able to run a function that takes a param", function() {
-        expect(c(r('{?a 1}(5)'))).toEqual(1);
+        expect(c(r(p('{?a 1}(5)')))).toEqual(1);
       });
 
       it("should be able use an arg passed to a lambda", function() {
-        expect(c(r('{?a a}(1)'))).toEqual(1);
+        expect(c(r(p('{?a a}(1)')))).toEqual(1);
       });
 
       it("should be able to invoke a lambda on several args used in the lambda", function() {
-        expect(c(r('{?a ?b add(a b)}(1 2)'))).toEqual(3);
+        expect(c(r(p('{?a ?b add(a b)}(1 2)')))).toEqual(3);
       });
 
       it("should be able to call a lambda nested many layers deep", function() {
-        expect(c(r('{ { { 1 } } }()()()'))).toEqual(1);
+        expect(c(r(p('{ { { 1 } } }()()()')))).toEqual(1);
       });
 
       it("should be able to execute lambda on arg from lambda invocation", function() {
-        expect(c(r("{1}({2}())"))).toEqual(1);
+        expect(c(r(p("{1}({2}())")))).toEqual(1);
       });
     });
   });
 
   describe("do blocks", function() {
     it("should be able to run an empty program", function() {
-      expect(c(r(""))).toBeUndefined();
+      expect(c(r(p("")))).toBeUndefined();
     });
 
     it("should return last expression in a top level do block", function() {
-      expect(c(r("1\n2\n3"))).toEqual(3);
+      expect(c(r(p("1\n2\n3")))).toEqual(3);
     });
 
     it("should return last expression in a lambda do block", function() {
-      expect(c(r("{ 1\n2\n3 }()"))).toEqual(3);
+      expect(c(r(p("{ 1\n2\n3 }()")))).toEqual(3);
     });
 
     it("should call all expressions in a top level do block", function() {
@@ -103,7 +104,7 @@ describe("interpreter", function() {
 
       var env = i.createScope(lib);
 
-      c(r("called()\ncalled()", env));
+      c(r(p("called()\ncalled()"), env));
       expect(callCount).toEqual(2);
     });
 
@@ -116,18 +117,18 @@ describe("interpreter", function() {
 
       var env = i.createScope(lib);
 
-      c(r("{ called()\ncalled() }()", env));
+      c(r(p("{ called()\ncalled() }()"), env));
       expect(callCount).toEqual(2);
     });
   });
 
   describe("assignments", function() {
     it("should return value assigned from assignment expression", function() {
-      expect(c(r("name: 1"))).toEqual(1);
+      expect(c(r(p("name: 1")))).toEqual(1);
     });
 
     it("should allow usage of bound value in top level", function() {
-      expect(c(r('name: "Lauren"\n  name'))).toEqual("Lauren");
+      expect(c(r(p('name: "Lauren"\n  name')))).toEqual("Lauren");
     });
 
     it("should not allow usage of bound value before binding in top level", function() {
@@ -140,12 +141,12 @@ describe("interpreter", function() {
 
       var env = i.createScope(lib);
 
-      c(r('pass(name)\nname: "Lauren"', env));
+      c(r(p('pass(name)\nname: "Lauren"'), env));
       expect(callCount).toEqual(1);
     });
 
     it("should allow usage of bound value in invoked lambda", function() {
-      expect(c(r('{ name: "Lauren"\n  name }()'))).toEqual("Lauren");
+      expect(c(r(p('{ name: "Lauren"\n  name }()')))).toEqual("Lauren");
     });
 
     it("should not allow usage of bound value before binding in invoked lambda", function() {
@@ -158,46 +159,46 @@ describe("interpreter", function() {
 
       var env = i.createScope(lib);
 
-      c(r('{ pass(name)\nname: "Lauren" }()', env));
+      c(r(p('{ pass(name)\nname: "Lauren" }()'), env));
       expect(callCount).toEqual(1);
     });
   });
 
   describe("if", function() {
     it("should return first branch if true", function() {
-      expect(c(r("if true { 1 }"))).toEqual(1);
+      expect(c(r(p("if true { 1 }")))).toEqual(1);
     });
 
     it("should return else if if is true", function() {
-      expect(c(r("if false { 1 } else { 2 }"))).toEqual(2);
+      expect(c(r(p("if false { 1 } else { 2 }")))).toEqual(2);
     });
 
     it("should return undefined if false and no second branch", function() {
-      expect(c(r("if false { 1 }"))).toBeUndefined();
+      expect(c(r(p("if false { 1 }")))).toBeUndefined();
     });
 
     it("should allow s-expression in branch", function() {
-      expect(c(r('if true { print("hi") }'))).toEqual("hi\n");
+      expect(c(r(p('if true { print("hi") }')))).toEqual("hi\n");
     });
 
     it("should return else if branch if condition true and if condition false", function() {
-      expect(c(r('if false { 1 } elseif true { 2 }'))).toEqual(2);
+      expect(c(r(p('if false { 1 } elseif true { 2 }')))).toEqual(2);
     });
 
     it("should return if branch if condition true and else if condition true", function() {
-      expect(c(r('if true { 1 } elseif true { 2 }'))).toEqual(1);
+      expect(c(r(p('if true { 1 } elseif true { 2 }')))).toEqual(1);
     });
 
     it("should return else branch if if and if else conditions false", function() {
-      expect(c(r('if false { 1 } elseif false { 2 } else { 3 }'))).toEqual(3);
+      expect(c(r(p('if false { 1 } elseif false { 2 } else { 3 }')))).toEqual(3);
     });
 
     it("should return second else branch if true and prev conditions false", function() {
-      expect(c(r('if false { 1 } elseif false { 2 } elseif true { 3 }'))).toEqual(3);
+      expect(c(r(p('if false { 1 } elseif false { 2 } elseif true { 3 }')))).toEqual(3);
     });
 
     it("should not return else if previous condition is true", function() {
-      expect(c(r('if false { 1 } elseif true { 2 } else { 3 }'))).toEqual(2);
+      expect(c(r(p('if false { 1 } elseif true { 2 } else { 3 }')))).toEqual(2);
     });
   });
 
@@ -211,7 +212,7 @@ describe("interpreter", function() {
 
       var env = i.createScope(lib);
 
-      c(r('b: { ?x called()\nif less-than(x 5) { b(add(x 1)) } }\nb(0)', env));
+      c(r(p('b: { ?x called()\nif less-than(x 5) { b(add(x 1)) } }\nb(0)'), env));
       expect(callCount).toEqual(6);
     });
 
@@ -225,12 +226,12 @@ describe("interpreter", function() {
         };
 
         var env = i.createScope(lib);
-        c(r('b: { ?n done(n)\nb(add(n 1)) }\nb(0)', env));
+        c(r(p('b: { ?n done(n)\nb(add(n 1)) }\nb(0)'), env));
       }).toThrow("Made it to 18000");
     });
 
     it("should trampoline a program where there is an if in the tail position", function() {
-      expect(c(r('countto: { ?x if greater-than(x 0) { countto(subtract(x 1) y) } else { "done"} }\ncountto(20000)'))).toEqual("done");
+      expect(c(r(p('countto: { ?x if greater-than(x 0) { countto(subtract(x 1) y) } else { "done"} }\ncountto(20000)')))).toEqual("done");
     });
   });
 });
