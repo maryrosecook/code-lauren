8487ffd1b021ad5442d4e351a4a51ad626863375
Mary Rose Cook <mary@maryrosecook.com>
2015-05-21 17:59:46 -0400

    Support marking char that is actually on next line on prev line

--- src/annotator.js
+++ src/annotator.js
@@ -14,9 +14,22 @@ function Annotator(editSession) {
   this.codeHighlight = function(code, i, clazz) {
     if (_.isNumber(i)) {
       var lAndC = parser.indexToLineAndColumn(i, code);
-      var r = new range.Range(lAndC.line - 1, lAndC.column - 1, lAndC.line - 1, lAndC.column);
+
+      // Add overhang if marked char is actually off end of line.
+      // This happens because Ace doesn't support extending markers
+      // outside text in this context and if we marked the first char
+      // of the next line it would look a bit weird to the user.
+      var overhangClass = isOffEndOfLine(code, i) ? " overhang" : "";
+
+      var r = new range.Range(lAndC.line - 1,
+                              lAndC.column - 1,
+                              lAndC.line - 1,
+                              lAndC.column);
+
       markerIds.push(editSession.addMarker(r,
-                                           "ace_code_highlight " + clazz,
+                                           "ace_code_highlight " +
+                                           clazz + " " +
+                                           overhangClass,
                                            "text",
                                            true));
     }
@@ -36,4 +49,10 @@ function Annotator(editSession) {
     markerIds = [];
     $("#line-messages").empty();
   };
+};
+
+function isOffEndOfLine(code, i) {
+  var lAndC = parser.indexToLineAndColumn(i, code);
+  var lineStr = code.split("\n")[lAndC.line - 1];
+  return lineStr === undefined || lineStr[lAndC.column - 1] === undefined;
 };
