145cdec312cabd8e3bcee97bfd8078b4b6ab3f75
Mary Rose Cook <mary@maryrosecook.com>
2015-06-04 22:43:54 -0400

    Flesh out some of stack for stack interpreter

--- src/lang/stack-interpreter.js
+++ src/lang/stack-interpreter.js
@@ -1,7 +1,19 @@
 function Stack() {
-
+  this.frames = [];
 };
 
 Stack.prototype = {
+  pushFrame: function(args) {
+    this.frames.push({
+      args: args
+    });
+  },
 
+  popFrame: function() {
+    return this.frames.pop();
+  }
 }
+
+function interpret() {
+
+};
