89c50bc349b4efca1a1db9215166073833e8d5b8
Mary Rose Cook <mary@maryrosecook.com>
2015-06-12 17:12:26 -0400

    Libs no longer use generators
    * Prep for using new compiler/vm that don’t use generators

--- src/lang/canvas-library.js
+++ src/lang/canvas-library.js
@@ -3,30 +3,30 @@ var _ = require("underscore");
 var canvasLibrary = module.exports = function(screen) {
   var drawOperations = [];
 
-  function* runCachedOperations() {
+  function runCachedOperations() {
     for (var i = 0; i < drawOperations.length; i++) {
-      yield* drawOperations[i]();
+      drawOperations[i]();
     }
 
     drawOperations = [];
   };
 
   return {
-    "write-text": function*(str, x, y, color) {
-      yield* runCachedOperations();
+    "write-text": function(str, x, y, color) {
+      runCachedOperations();
       screen.font = "20px Georgia";
       screen.fillStyle = color;
       screen.fillText(str, x, y);
     },
 
-    "clear-screen": function*() {
-      drawOperations.push(function*() {
+    "clear-screen": function() {
+      drawOperations.push(function() {
         screen.clearRect(0, 0, screen.canvas.width, screen.canvas.height);
       });
     },
 
-    "draw-unfilled-circle": function*(x, y, radius, color) {
-      yield* runCachedOperations();
+    "draw-unfilled-circle": function(x, y, radius, color) {
+      runCachedOperations();
       screen.beginPath();
       screen.arc(x, y, radius, 0, Math.PI * 2, true);
       screen.closePath();
@@ -34,8 +34,8 @@ var canvasLibrary = module.exports = function(screen) {
       screen.stroke();
     },
 
-    "draw-filled-circle": function*(x, y, radius, color) {
-      yield* runCachedOperations();
+    "draw-filled-circle": function(x, y, radius, color) {
+      runCachedOperations();
       screen.beginPath();
       screen.arc(x, y, radius, 0, Math.PI * 2, true);
       screen.closePath();
@@ -43,14 +43,14 @@ var canvasLibrary = module.exports = function(screen) {
       screen.fill();
     },
 
-    "draw-unfilled-rectangle": function*(x, y, width, height, color) {
-      yield* runCachedOperations();
+    "draw-unfilled-rectangle": function(x, y, width, height, color) {
+      runCachedOperations();
       screen.strokeStyle = color;
       screen.strokeRect(x, y, width, height);
     },
 
-    "draw-filled-rectangle": function*(x, y, width, height, color) {
-      yield* runCachedOperations();
+    "draw-filled-rectangle": function(x, y, width, height, color) {
+      runCachedOperations();
       screen.fillStyle = color;
       screen.fillRect(x, y, width, height);
     }
--- src/lang/standard-library.js
+++ src/lang/standard-library.js
@@ -3,81 +3,82 @@ var interpreter = require("./interpreter");
 
 var createStandardLibrary = module.exports = function () {
   var lib = {
-    add: function*() {
+    add: function() {
       return _.reduce(arguments, function(a, n) { return a + n; });
     },
 
-    subtract: function*() {
+    subtract: function() {
       return _.reduce(arguments, function(a, n) { return a - n; });
     },
 
-    multiply: function*() {
+    multiply: function() {
       return _.reduce(arguments, function(a, n) { return a * n; });
     },
 
-    sine: function*(x) {
+    sine: function(x) {
       return Math.sin(lib.radians(x).next().value);
     },
 
-    cosine: function*(x) {
+    cosine: function(x) {
       return Math.cos(lib.radians(x).next().value);
     },
 
-    tangent: function*(x) {
+    tangent: function(x) {
       return Math.tan(lib.radians(x).next().value);
     },
 
-    radians: function*(x) {
+    radians: function(x) {
       return 0.01745 * x;
     },
 
-    degrees: function*(x) {
+    degrees: function(x) {
       return x / 0.01745;
     },
 
-    "new-dictionary": function*() {
+    "new-dictionary": function() {
       return _.object(_.filter(arguments, function(_, i) { return i % 2 === 0; }),
                       _.filter(arguments, function(_, i) { return i % 2 === 1; }));
     },
 
-    "less-than": function*(a, b) {
+    "less-than": function(a, b) {
       return a < b;
     },
 
-    "greater-than": function*(a, b) {
+    "greater-than": function(a, b) {
       return a > b;
     },
 
-    equals: function*() {
+    equals: function() {
       var args = _.toArray(arguments);
       if (args.length < 2 || args[0] !== args[1]) {
         return false;
       } else if (args.length === 2) {
         return true;
       } else {
-        return yield* lib.equals.apply(null, args.slice(1));
+        return lib.equals.apply(null, args.slice(1));
       }
     },
 
-    set: function*(dict, key, value) {
+    set: function(dict, key, value) {
       dict[key] = value;
       return dict;
     },
 
-    get: function*(dict, key) {
+    get: function(dict, key) {
       return dict[key];
     },
 
-    print: function*() {
+    print: function(printable) {
       var output = _.map(arguments, function(x) { return x.toString(); }).join(" ");
       console.log(output);
       return output + "\n";
     },
 
-    forever: function*(fn) {
-      while (true) {
-        yield* require("./interpreter").trampoline(yield* fn());
-      }
+    forever: function(fn) {
+      throw "broken for now until trampolining brought back!"
+      // while (true) {
+      //   yield* require("./interpreter").trampoline(yield* fn());
+      // }
     }
   };
 
