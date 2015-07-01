6dbc5e10664c26793ee321efa41d9ce0503c7a5e
Mary Rose Cook <mary@maryrosecook.com>
2015-07-01 17:26:53 -0400

    Only annotate instructions that make sense when stepping
    * Also only pause on and step to annotatable instruction

--- src/player-interface.jsx
+++ src/player-interface.jsx
@@ -1,11 +1,26 @@
 var React = require('react');
 
+var instructionsToNotAnnotate = ["pop", "return", "jump", "if_not_true_jump"];
+
 function annotateCurrentInstruction(p, annotator) {
   annotator.clear();
   annotator.codeHighlight(p.code,
                           p.currentInstruction.ast.s,
                           p.currentInstruction.ast.e,
                           "currently-executing");
+};
+
+function isAnnotatableInstruction(ins) {
+  console.log(ins);
+  return instructionsToNotAnnotate.indexOf(ins[0]) === -1;
+};
+
+function stepUntilReachAnnotatableInstruction(player) {
+  var currentInstruction = player.getProgram().currentInstruction;
+  while (!isAnnotatableInstruction(currentInstruction)) {
+    player.stepForwards(1);
+    currentInstruction = player.getProgram().currentInstruction;
+  }
 };
 
 var ProgramPlayer = React.createClass({
@@ -15,18 +30,21 @@ var ProgramPlayer = React.createClass({
 
   onPlayPauseClick: function() {
     this.state.player.togglePause();
-    this.setState(this.state);
 
     if (this.state.player.isPaused()) {
+      stepUntilReachAnnotatableInstruction(this.state.player);
       annotateCurrentInstruction(this.state.player.getProgram(), this.props.annotator);
     } else {
       this.props.annotator.clear();
     }
+
+    this.setState(this.state);
   },
 
   onStepForwardsClick: function() {
     this.state.player.pause();
     this.state.player.stepForwards(1);
+    stepUntilReachAnnotatableInstruction(this.state.player);
     this.setState(this.state);
 
     annotateCurrentInstruction(this.state.player.getProgram(), this.props.annotator);
