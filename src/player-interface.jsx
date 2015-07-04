8a97d0f02e4c4412efc908e9434e2cdd7b668a3a
Mary Rose Cook <mary@maryrosecook.com>
2015-07-04 15:17:19 -0400

    Don’t print out instructions when stepping

--- src/player-interface.jsx
+++ src/player-interface.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var compiler = require("./lang/compiler");
 
 function annotateCurrentInstruction(p, annotator) {
-  console.log(p.currentInstruction);
   annotator.clear();
   annotator.codeHighlight(p.code,
                           p.currentInstruction.ast.s,
