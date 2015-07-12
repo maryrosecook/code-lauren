c55a6ed99d248b73f40a2dda9ee45e3cd1de7e56
Mary Rose Cook <mary@maryrosecook.com>
2015-07-12 00:42:50 -0400

    Make build-pages script that rebuilds on pages/ change

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -4,17 +4,27 @@ var marked = require("marked");
 
 var PAGES_PATH = __dirname + "/../pages";
 
-var pages = fs.readdirSync(PAGES_PATH)
-    .filter(function(n) { return n.match(/\.md$/); })
-    .map(function(n) { return path.join(PAGES_PATH, n); })
-    .map(function(filePath) {
-      return {
-        name: filePath.match(/^.+\/([A-Za-z0-9-]+)\.md$/)[1],
-        string: marked(fs.readFileSync(filePath, "utf8"))
-      };
-    }).reduce(function(a, o) {
-      a[o.name] = o.string;
-      return a
-    }, {});
+function buildPages() {
+  var pages = fs.readdirSync(PAGES_PATH)
+      .filter(function(n) { return n.match(/\.md$/); })
+      .map(function(n) { return path.join(PAGES_PATH, n); })
+      .map(function(filePath) {
+        return {
+          name: filePath.match(/^.+\/([A-Za-z0-9-]+)\.md$/)[1],
+          string: marked(fs.readFileSync(filePath, "utf8"))
+        };
+      }).reduce(function(a, o) {
+        a[o.name] = o.string;
+        return a
+      }, {});
 
-fs.writeFileSync(path.join(PAGES_PATH, "/pages.json"), JSON.stringify(pages));
+  fs.writeFileSync(path.join(PAGES_PATH, "/all-pages.js"),
+                   "module.exports = " + JSON.stringify(pages));
+};
+
+// rebuild pages when something in on change
+fs.watch(__dirname + "/../pages", function (event, filename) {
+  if (filename.match(/\.md$/)) {
+    buildPages();
+  }
+});
