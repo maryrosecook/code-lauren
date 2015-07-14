2056d22e4cd6960b2f2d0b2dce5bc3dd6578f039
Mary Rose Cook <mary@maryrosecook.com>
2015-07-14 13:33:59 -0400

    Rewrite doc markdown links as onClicks

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -1,17 +1,18 @@
 var fs = require("fs");
 var path = require("path");
+var _ = require("underscore");
 var marked = require("marked");
 
 var PAGES_PATH = __dirname + "/../pages";
 
-function buildPages() {
+(function buildPages() {
   var pages = fs.readdirSync(PAGES_PATH)
       .filter(function(n) { return n.match(/\.md$/); })
       .map(function(n) { return path.join(PAGES_PATH, n); })
       .map(function(filePath) {
         return {
           name: filePath.match(/^.+\/([A-Za-z0-9-]+)\.md$/)[1],
-          string: marked(fs.readFileSync(filePath, "utf8"))
+          string: marked(makeLinksOnClick(fs.readFileSync(filePath, "utf8")))
         };
       }).reduce(function(a, o) {
         a[o.name] = o.string;
@@ -20,6 +21,50 @@ function buildPages() {
 
   fs.writeFileSync(path.join(PAGES_PATH, "/all-pages.js"),
                    "module.exports = " + JSON.stringify(pages));
+})();
+
+function matchIndices(regex, str) {
+  var match;
+  var indices = [];
+  var regex = new RegExp(regex, 'g');
+
+  while (match = regex.exec(str)) {
+    indices.push([match.index, match.index+match[0].length]);
+  }
+
+  return indices;
+};
+
+function maybeMarkdownLinkToOnClick(link) {
+  var pageName = link.match(/\(#([^\)]+)\)$/);
+  if (pageName) {
+    var text = link.match(/^\[([^\]]+)\]/);
+    return ['<a href=""',
+            'onclick="return sidebar.load(',
+            "'", pageName[1], "'",
+            ');"',
+            '>',
+            text[1],
+            "</a>"].join("");
+  } else {
+    return link;
+  }
+};
+
+function makeLinksOnClick(md) {
+  var linkIndices = matchIndices("\\[[^\\]]+\\]\\(#[^)]+\\)", md);
+  var links = linkIndices
+      .map(function(range) { return md.slice(range[0], range[1]); })
+      .map(maybeMarkdownLinkToOnClick);
+
+  var allIndices = [[undefined, 0]].concat(linkIndices).concat([[md.length]]);
+
+  var nonLinks = [];
+  for (var i = 1; i < allIndices.length; i++) {
+    nonLinks.push(md.slice(allIndices[i - 1][1], allIndices[i][0]));
+  }
+
+  return _.compact(_.flatten(_.zip(nonLinks, links))).join("");
 };
 
 // rebuild pages when something in on change
