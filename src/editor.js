831d96e64cb53f5337936d63b82483739e4f0a8a
Mary Rose Cook <mary@maryrosecook.com>
2015-07-17 17:33:51 -0400

    Make editor take focus if user starts typing

--- src/editor.js
+++ src/editor.js
@@ -1,3 +1,5 @@
+var $ = require("jquery");
+
 var CodeMirror = require('codemirror');
 require("./lib/simplescrollbars.js"); // for codemirror scrollbars
 require('./mode-lauren');
@@ -13,6 +15,11 @@ var createEditor = module.exports = function() {
     extraKeys: {
       Tab: function(cm) { cm.execCommand("insertSoftTab"); }
     }
+  });
+
+  // restore focus to editor if user ever starts typing
+  $(window).on("keydown", function() {
+    editor.focus();
   });
 
   return editor;
