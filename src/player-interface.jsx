3fa3931e1d78b13a56ba0452f4650b7e3edb0ec2
Mary Rose Cook <mary@maryrosecook.com>
2015-08-12 14:54:48 -0400

    Player buttons disabled if operation not possible

--- src/player-interface.jsx
+++ src/player-interface.jsx
@@ -1,14 +1,24 @@
 var $ = require('jquery');
 var React = require('react');
-var compiler = require("./lang/compiler");
+var _ = require("underscore");
+
 var vm = require("./lang/vm");
-var setupPlayer = require("./program-player");
 var env = require("./env");
 var vm = require("./lang/vm");
 var parser = require("./lang/parser");
 var compile = require("./lang/compiler");
+var langUtil = require("../src/lang/lang-util");
+var copyProgramState = require("../src/copy-program-state");
+
+var STEP_TO_SAVE = 5000;
+
+function isTimeToYieldToEventLoop(lastYield) {
+  return new Date().getTime() - lastYield > 8;
+};
 
 function annotateCurrentInstruction(ps, annotator) {
+  if (ps.currentInstruction === undefined) { return; }
+
   annotator.clear();
   annotator.codeHighlight(ps.code,
                           ps.currentInstruction.ast.s,
@@ -79,77 +89,191 @@ function displayRainbowParentheses(code, annotator) {
 
 var ProgramPlayer = React.createClass({
   getInitialState: function() {
-    this.stepBackwardsClickHandler = onClickOrHoldDown(this.stepBackwards);
-    this.stepForwardsClickHandler = onClickOrHoldDown(this.stepForwards);
-
-    var player = setupPlayer(this.props.annotator);
-
     var self = this;
-    this.props.editor.on("change", function() {
-      player.setProgramState(initProgramState(self.props.editor.getValue(),
-                                              self.props.annotator,
-                                              self.props.canvasLib));
 
+    (function tick(lastEventLoopYield) {
+      while(true) {
+        if (self.state !== null && self.state.ps !== undefined && !self.state.paused) {
+          if (vm.isComplete(self.state.ps)) {
+            self.state.ps.canvasLib.flush();
+          } else {
+            self.stepForwards();
+          }
+        }
+
+        if (isTimeToYieldToEventLoop(lastEventLoopYield)) {
+          requestAnimationFrame(() => tick(new Date().getTime()));
+          break;
+        }
+      }
+    })(new Date().getTime());
+
+    this.stepBackwardsClickHandler = onClickOrHoldDown(this.stepBackwardsByHand);
+    this.stepForwardsClickHandler = onClickOrHoldDown(this.stepForwardsByHand);
+
+
+    this.props.editor.on("change", function() {
+      self.state.ps = initProgramState(self.props.editor.getValue(),
+                                       self.props.annotator,
+                                       self.props.canvasLib);
+      self.state.paused = false;
+      self.state.pses = [];
       self.setState(self.state);
     });
 
-    return { player: player };
+    return { ps: undefined, paused: false, pses: [] };
   },
 
   onRewindClick: function() {
-    this.state.player.setProgramState(initProgramState(this.props.editor.getValue(),
-                                                       this.props.annotator,
-                                                       this.props.canvasLib));
-    this.state.player.pause();
-    this.stepForwards();
+    this.state.ps = initProgramState(this.props.editor.getValue(),
+                                     this.props.annotator,
+                                     this.props.canvasLib);
+    this.state.pses = [];
+    this.stepForwardsByHand();
   },
 
   onPlayPauseClick: function() {
-    this.state.player.togglePause();
-
-    if (this.state.player.isPaused()) {
-      annotateCurrentInstruction(this.state.player.getProgramState(), this.props.annotator);
+    if (this.state.paused === true) {
+      this.unpause();
+    } else if (this.state.paused === false) {
+      this.pause();
     } else {
-      this.props.annotator.clear();
+      throw new Error("Tried to toggle player pause but it's neither paused nor unpaused");
     }
+  },
 
+  pause: function() {
+    this.state.paused = true;
+    this.state.ps.canvasLib.pause();
+    annotateCurrentInstruction(this.state.ps, this.props.annotator);
     this.setState(this.state);
+  },
+
+  unpause: function() {
+    this.state.paused = false;
+    this.props.annotator.clear();
+    this.setState(this.state);
+  },
+
+  stepForwardsByHand: function() {
+    this.stepForwards();
+    this.pause();
   },
 
   stepForwards: function() {
-    this.state.player.pause();
-    this.state.player.stepForwards();
-    this.setState(this.state);
+    if (vm.isCrashed(this.state.ps) || vm.isComplete(this.state.ps)) {
+      this.setState(this.state); // update buttons
+      return;
+    }
 
-    annotateCurrentInstruction(this.state.player.getProgramState(), this.props.annotator);
+    var originalPs = this.state.ps;
+    var newPses = [];
+    while (true) {
+      try {
+        newPses.push(copyProgramState(this.state.ps));
+        vm.step(this.state.ps);
+      } catch (e) {
+        if (e instanceof langUtil.RuntimeError) {
+          this.props.annotator.codeHighlight(this.state.ps.code, e.s, e.e, "error");
+          this.props.annotator.lineMessage(this.state.ps.code, e.s, "error", e.message);
+        }
+      }
+
+      if (vm.isComplete(this.state.ps)) {
+        newPses = [];
+        this.state.ps = originalPs;
+        break;
+      } else if (vm.isCrashed(this.state.ps) ||
+                 (this.state.ps.currentInstruction !== undefined &&
+                  this.state.ps.currentInstruction.annotate === compile.ANNOTATE)) {
+        break;
+      }
+    }
+
+    for (var i = 0; i < newPses.length; i++) {
+      if (this.state.pses.length > STEP_TO_SAVE) {
+        this.state.pses.shift();
+        this.state.ps.canvasLib.deleteOld(STEP_TO_SAVE);
+      }
+
+      newPses[i].canvasLib.stepForwards();
+      this.state.pses.push(newPses[i]);
+    }
   },
 
   stepBackwards: function() {
-    this.state.player.pause();
-    this.state.player.stepBackwards();
-    this.setState(this.state);
+    var originalPs = this.state.ps;
+    var i = this.state.pses.length - 1;
+    while (true) {
+      if (i === -1) { // couldn't find annotatable previous ps - don't step back
+        this.state.ps = originalPs;
+        this.setState(this.state); // update buttons
+        break;
+      } else if (this.state.pses[i].currentInstruction !== undefined &&
+                 this.state.pses[i].currentInstruction.annotate === compile.ANNOTATE) {
+        for (var j = this.state.pses.length - 1; j >= i; j--) {
+          this.state.ps = this.state.pses.pop();
+          this.state.ps.canvasLib.stepBackwards();
+        }
 
-    annotateCurrentInstruction(this.state.player.getProgramState(), this.props.annotator);
+        break;
+      }
+
+      i--;
+    }
+  },
+
+  stepBackwardsByHand: function() {
+    this.stepBackwards();
+    this.pause();
+  },
+
+  canStepForwards: function() {
+    if (this.state.ps === undefined) { return false; }
+
+    var testPs = copyProgramState(this.state.ps);
+    while(true) {
+      try {
+        vm.step(testPs);
+      } catch(e) {} // swallow any errors - program will get marked as crashed
+
+      if (testPs.currentInstruction !== undefined &&
+          testPs.currentInstruction.annotate === compile.ANNOTATE) {
+        return true;
+      } else if (vm.isCrashed(testPs) || vm.isComplete(testPs)) {
+        return false;
+      }
+    }
+  },
+
+  canStepBackwards: function() {
+    return this.state.pses !== undefined && this.state.pses.length > 1;
   },
 
   render: function() {
     var playPauseClassName = "player-button " +
-        (this.state.player.isPaused() ? "play-button" : "pause-button");
+        (this.state.paused ? "play-button" : "pause-button");
+
+    var stepBackwardsEnabledClassName = !this.state.paused || this.canStepBackwards() ?
+        "" : " disabled ";
+    var stepForwardsEnabledClassName = !this.state.paused || this.canStepForwards() ?
+        "" : " disabled ";
 
     return (
         <div className="program-player">
           <button onClick={this.onRewindClick}
-                  className="player-button rewind-button" />
+                  className={"player-button rewind-button" + stepBackwardsEnabledClassName} />
 
           <button onMouseDown={this.stepBackwardsClickHandler}
                   onMouseUp={this.stepBackwardsClickHandler}
-                  className="player-button step-backwards-button" />
+                  className={"player-button step-backwards-button" +
+                             stepBackwardsEnabledClassName} />
 
           <button onClick={this.onPlayPauseClick} className={playPauseClassName} />
 
           <button onMouseDown={this.stepForwardsClickHandler}
                   onMouseUp={this.stepForwardsClickHandler}
-                  className="player-button step-forwards-button" />
+                  className={"player-button step-forwards-button" + stepForwardsEnabledClassName} />
         </div>
     );
   }
