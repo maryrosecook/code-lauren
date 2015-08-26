483cca9d8cfd4f05bf60e5a5af3a51ff9d099536
Mary Rose Cook <mary@maryrosecook.com>
2015-08-26 17:06:03 -0400

    Add argument type checking for all std lib functions

--- spec/standard-library.spec.js
+++ spec/standard-library.spec.js
@@ -44,10 +44,123 @@ describe("library", function() {
 
     it("should throw if missing args", function() {
       var code = "add()";
-      expect(function() { v(code, c(p(code))) }).toThrow("a number to add");
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers");
 
       var code = "add(1)";
-      expect(function() { v(code, c(p(code))) }).toThrow("a number to add");
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to add");
+    });
+  });
+
+  describe("subtract", function() {
+    it("should be able to subtract two values", function() {
+      expect(standardLibrary().subtract({}, 2, 1)).toEqual(1);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "subtract()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers");
+
+      var code = "subtract(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to subtract");
+    });
+  });
+
+  describe("multiply", function() {
+    it("should be able to multiply two values", function() {
+      expect(standardLibrary().multiply({}, 4, 2)).toEqual(8);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "multiply()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers to multiply");
+
+      var code = "multiply(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to multiply by");
+    });
+  });
+
+  describe("divide", function() {
+    it("should be able to multiply two values", function() {
+      expect(standardLibrary().divide({}, 8, 2)).toEqual(4);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "divide()";
+      expect(function() { v(code, c(p(code))) })
+        .toThrow("Missing a number to divide and a number to divide by");
+
+      var code = "divide(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to divide by");
+    });
+  });
+
+  describe("modulus", function() {
+    it("should be able to modulo one value by another", function() {
+      expect(standardLibrary().modulus({}, 8, 2)).toEqual(0);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "modulus()";
+      expect(function() { v(code, c(p(code))) })
+        .toThrow("Missing a number to divide and a number to divide by");
+
+      var code = "modulus(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to divide by");
+    });
+  });
+
+  describe("sine", function() {
+    it("should be able to get sine of angle", function() {
+      expect(standardLibrary().sine({}, 0)).toEqual(0);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "sine()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing an angle to get the sine of");
+    });
+  });
+
+  describe("cosine", function() {
+    it("should be able to get sine of angle", function() {
+      expect(standardLibrary().cosine({}, 0)).toEqual(1);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "cosine()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing an angle to get the cosine of");
+    });
+  });
+
+  describe("tangent", function() {
+    it("should be able to get tangent of angle", function() {
+      expect(standardLibrary().cosine({}, 0)).toEqual(1);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "tangent()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing an angle to get the tangent of");
+    });
+  });
+
+  describe("radians", function() {
+    it("should be able to get angle in radians", function() {
+      expect(standardLibrary().radians({}, 360)).toEqual(6.282);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "radians()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing an angle to convert to radians");
+    });
+  });
+
+  describe("degrees", function() {
+    it("should be able to get angle in radians", function() {
+      expect(standardLibrary().radians({}, 360)).toEqual(6.282);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "radians()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing an angle to convert to radians");
     });
   });
 
@@ -71,8 +184,12 @@ describe("library", function() {
       expect(standardLibrary()["less-than"]({}, 2, 1)).toEqual(false);
     });
 
-    it("should return false for no args", function() {
-      expect(standardLibrary()["less-than"]()).toEqual(false);
+    it("should throw if missing args", function() {
+      var code = "less-than()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers");
+
+      var code = "less-than(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a second number");
     });
   });
 
@@ -85,14 +202,46 @@ describe("library", function() {
       expect(standardLibrary()["greater-than"]({}, 1, 2)).toEqual(false);
     });
 
-    it("should return false for no args", function() {
-      expect(standardLibrary()["greater-than"]()).toEqual(false);
+    it("should throw if missing args", function() {
+      var code = "greater-than()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers");
+
+      var code = "greater-than(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a second number");
     });
   });
 
   describe("equal", function() {
     it("should return true for two identical args", function() {
-      expect(standardLibrary()["equal"]({}, 1, 1)).toEqual(true);
+      expect(standardLibrary().equal({}, 1, 1)).toEqual(true);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "equal()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing two numbers");
+
+      var code = "equal(1)";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a second number");
+    });
+  });
+
+  describe("counted", function() {
+    it("should be able to count to three twice times", function() {
+      var lib = standardLibrary();
+      var meta = { ast: { s: 10 } };
+
+      expect(lib.counted(meta, 3)).toEqual(false);
+      expect(lib.counted(meta, 3)).toEqual(false);
+      expect(lib.counted(meta, 3)).toEqual(true);
+
+      expect(lib.counted(meta, 3)).toEqual(false);
+      expect(lib.counted(meta, 3)).toEqual(false);
+      expect(lib.counted(meta, 3)).toEqual(true);
+    });
+
+    it("should throw if missing args", function() {
+      var code = "counted()";
+      expect(function() { v(code, c(p(code))) }).toThrow("Missing a number to count to");
     });
   });
 });
--- src/lang/standard-library.js
+++ src/lang/standard-library.js
@@ -6,44 +6,75 @@ var createStandardLibrary = module.exports = function () {
   var lib = {
     add: function(meta, a, b) {
       chk(arguments,
-          chk.num("a number to add"),
-          chk.num("a number to add"));
+          chk.num("Missing two numbers"),
+          chk.num("Missing a number to add"));
       return parseFloat(a) + parseFloat(b);
     },
 
     subtract: function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing two numbers"),
+          chk.num("Missing a number to subtract"));
+
       return a - b;
     },
 
     multiply: function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing two numbers to multiply"),
+          chk.num("Missing a number to multiply by"));
+
       return a * b;
     },
 
     divide: function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing a number to divide and a number to divide by"),
+          chk.num("Missing a number to divide by"));
+
       return a / b;
     },
 
     modulus: function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing a number to divide and a number to divide by"),
+          chk.num("Missing a number to divide by"));
+
       return a % b;
     },
 
     sine: function(meta, x) {
-      return Math.sin(lib.radians(x));
+      chk(arguments,
+          chk.num("Missing an angle to get the sine of"));
+
+      return Math.sin(lib.radians(meta, x));
     },
 
     cosine: function(meta, x) {
-      return Math.cos(lib.radians(x));
+      chk(arguments,
+          chk.num("Missing an angle to get the cosine of"));
+
+      return Math.cos(lib.radians(meta, x));
     },
 
     tangent: function(meta, x) {
-      return Math.tan(lib.radians(x));
+      chk(arguments,
+          chk.num("Missing an angle to get the tangent of"));
+
+      return Math.tan(lib.radians(meta, x));
     },
 
     radians: function(meta, x) {
+      chk(arguments,
+          chk.num("Missing an angle to convert to radians"));
+
       return 0.01745 * x;
     },
 
     degrees: function(meta, x) {
+      chk(arguments,
+          chk.num("Missing some radians to convert to degrees"));
+
       return x / 0.01745;
     },
 
@@ -54,14 +85,26 @@ var createStandardLibrary = module.exports = function () {
     },
 
     "less-than": function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing two numbers"),
+          chk.num("Missing a second number"));
+
       return a < b;
     },
 
     "greater-than": function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing two numbers"),
+          chk.num("Missing a second number"));
+
       return a > b;
     },
 
     equal: function(meta, a, b) {
+      chk(arguments,
+          chk.num("Missing two numbers"),
+          chk.num("Missing a second number"));
+
       return a === b;
     },
 
@@ -85,6 +128,9 @@ var createStandardLibrary = module.exports = function () {
 
     counters: [],
     counted: function(meta, target) {
+      chk(arguments,
+          chk.num("Missing a number to count to"));
+
       var counter = lib.counters[meta.ast.s];
       if (counter === undefined) {
         counter = lib.counters[meta.ast.s] = { count: 0, target: target };
